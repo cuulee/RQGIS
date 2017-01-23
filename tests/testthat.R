fa5637e75eea265b3cb58eb10182154d4a087a13
pat-s <patrick.schratz@gmail.com>
2017-01-23 22:36:01 +0100

    update testthat.R: try Travis fix

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,6 @@
-library("testthat")
+# possible solution to failing tests on Travis CI (cannot open the connection)
+# See https://github.com/hadley/testthat/issues/86
+Sys.setenv("R_TESTS" = "") 
 library("RQGIS")
 
 test_check("RQGIS")
